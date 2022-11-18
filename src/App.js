6e8c26ba9b66d02ee3822bea5ea78cf2f1a504fb
WaghAshu03 <waghashutosh03@gmail.com>
2022-11-19 01:06:05 +0530

    Setting up React Router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <h1>React.js Hooks</h1>
+      <nav>
+        <Link to="/reactjs-hooks/">Home</Link> <br />
+        <Link to="/reactjs-hooks/useState">useState</Link>
+      </nav>
+      <Routes>
+        <Route path="/reactjs-hooks/" element={<>Home</>} />
+        <Route path="/reactjs-hooks/useState" element={<>useState</>} />
+      </Routes>
     </div>
   );
 }
